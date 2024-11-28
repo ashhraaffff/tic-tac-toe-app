0e4005a9e4d1d577e482889dc7e35ec37a7fe1c0
AdiTailor <aditya.tailor@outlook.com>
2024-11-28 17:44:05 +0530

    add JUnit Tests

--- app/src/test/java/androidsamples/java/tictactoe/ExampleUnitTest.java
+++ app/src/test/java/androidsamples/java/tictactoe/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package androidsamples.java.tictactoe;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +14,33 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void win_check(){
+        GameFragment gm=new GameFragment();
+        String[] inputText = new String[]{"X", "X", "X", "O", "O", "", "", "", ""};
+        gm.setBoard(inputText);
+        assertEquals(gm.checkWin(),1);
+    }
+    @Test
+    public void draw_check(){
+        GameFragment gm=new GameFragment();
+        String[] inputText = new String[]{"O", "O", "X", "X", "X", "O", "O", "X", "O"};
+        gm.setBoard(inputText);
+        assertEquals(gm.checkWin(),0);
+    }
+    @Test
+    public void diagonal_win_check(){
+        GameFragment gm=new GameFragment();
+        String[] inputText = new String[]{"X", "O", "X", "O", "X", "0", "X", "O", "X"};
+        gm.setBoard(inputText);
+        assertEquals(gm.checkWin(),1);
+    }
+    @Test
+    public void loss_check(){
+        GameFragment gm=new GameFragment();
+        String[] inputText = new String[]{"X", "X", "", "O", "O", "O", "", "", "X"};
+        gm.setBoard(inputText);
+        assertEquals(gm.checkWin(),-1);
+    }
+
 }
